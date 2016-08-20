2995e520d2d97e0fcfc4dfc05d3945851a455d56
Jan C <jan@ruken.pw>
2016-08-20 17:21:27 +0200

    spawn threads on demand

--- src/com/janchr/Crawler.java
+++ src/com/janchr/Crawler.java
@@ -1,167 +1,163 @@
-package com.janchr;
+    package com.janchr;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.net.MalformedURLException;
+    import java.net.URL;
+    import java.util.*;
+    import java.util.regex.Matcher;
+    import java.util.regex.Pattern;
 
-/**
- * Created by Jan on 8/20/2016.
- */
+    /**
+     * Created by Jan on 8/20/2016.
+     */
 
-class CrawlThread implements Runnable {
-    final static Pattern urlPat = Pattern.compile("(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
+    class CrawlThread implements Runnable {
+        final static Pattern urlPat = Pattern.compile("(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
 
-    Crawler c;
-    int num;
-    boolean stop;
-    public Thread t;
+        Crawler c;
+        String url;
+        public Thread t;
 
-    public CrawlThread(Crawler c, int num) {
-        this.c = c;
-        this.num = num;
-        this.t = new Thread(this, "CrawlThread");
-        t.start();
-    }
+        public CrawlThread(Crawler c, String url) {
+            this.c = c;
+            this.url = url;
+            this.t = new Thread(this, "CrawlThread");
+            t.start();
+        }
 
-    private LinkedList<String> parse(BufferedReader r) {
-        String lineBuf = "";
-        LinkedList<String> urls = new LinkedList<String>();
-        do {
-            try {
-                lineBuf = r.readLine();
-            } catch (IOException e) {
-                System.out.println("(" + this.num + ") error parsing: " + e);
-                return urls;
-            }
-            if (lineBuf == null) {
-                return urls;
-            }
+        private LinkedList<String> parse(BufferedReader r) {
+            String lineBuf = "";
+            LinkedList<String> urls = new LinkedList<String>();
+            do {
+                try {
+                    lineBuf = r.readLine();
+                } catch (IOException e) {
+                    System.out.println("error parsing: " + e);
+                    return urls;
+                }
+                if (lineBuf == null) {
+                    return urls;
+                }
 
-            Matcher m = urlPat.matcher(lineBuf);
-            while(m.find()) {
-                //System.out.println("(" + this.num + ") match: " + m.group(0));
-                urls.add(m.group(0));
-            }
+                Matcher m = urlPat.matcher(lineBuf);
+                while(m.find()) {
+                    urls.add(m.group(0));
+                }
 
-        } while(lineBuf != null);
-        return urls;
-    }
+            } while(lineBuf != null);
+            return urls;
+        }
 
-    public void run() {
-        // pop_front the next URL and get it
-        do {
-            String surl = c.next();
-            //System.out.println("(" + this.num + ") getting " + surl);
-
+        public void run() {
             URL url;
             try {
-                url = new URL(surl);
+                url = new URL(this.url);
             } catch (MalformedURLException e) {
-                System.out.println("(" + this.num + ") bad url " + surl + ": " + e);
-                continue;
+                System.out.println("bad url " + this.url + ": " + e);
+                c.done(this, this.url);
+                return;
             }
 
             BufferedReader r;
             try {
                 r = Http.Get(url);
             } catch (IOException e) {
-                System.out.println("(" + this.num + ") IOException Http.Get " + surl + ": " + e);
-                continue;
+                System.out.println("IOException Http.Get " + this.url + ": " + e);
+                c.done(this, this.url);
+                return;
             }
-            c.done(surl);
 
             for (String newUrl: this.parse(r)) {
                 c.addURL(newUrl);
             }
-
-        } while(!this.stop);
-    }
-}
-
-class VisitedURL {
-    public String url;
-    public int visits;
-
-    VisitedURL(String url) {
-        this.url = url;
-    }
-}
-
-public class Crawler {
-    private List<String> queue = Collections.synchronizedList(new LinkedList<>());
-
-    private Map<String, VisitedURL> visited = Collections.synchronizedMap(new LinkedHashMap<>());
-    private ArrayList<CrawlThread> threads = new ArrayList<>();
-    private int maxThreads;
-
-    public Crawler(int maxThreads) {
-        this.maxThreads = maxThreads;
-    }
-
-    public void start(String entryPoint) {
-        this.queue.add(entryPoint);
-
-        for (int i = 0; i < this.maxThreads; i++) {
-            this.threads.add(new CrawlThread(this, i));
+            c.done(this, this.url);
         }
     }
 
-    public synchronized void stop() {
-        for(CrawlThread t: this.threads) {
-            // interrupting the thread should be fine for us in our use-case.
-            t.stop = true;
-            t.t.interrupt();
+    class VisitedURL {
+        public String url;
+        public int visits;
+
+        VisitedURL(String url) {
+            this.url = url;
         }
     }
 
-    public synchronized String next() {
-        // I got IndexOutOfBoundsException here when starting up the crawler.
-        // the only way to fix it for me was this loop. I don't know what would
-        // be a better way to fix it. A mutex didn't work for me.
-        do {
-            if (this.queue.size() == 0) {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+    public class Crawler {
+        private List<String> queue = Collections.synchronizedList(new LinkedList<>());
+
+        private Map<String, VisitedURL> visited = Collections.synchronizedMap(new LinkedHashMap<>());
+        private ArrayList<CrawlThread> threads = new ArrayList<>();
+        private int maxThreads;
+
+        public Crawler(int maxThreads) {
+            this.maxThreads = maxThreads;
+        }
+
+        public void start(String entryPoint) {
+            this.queue.add(entryPoint);
+
+            this.tryNext();
+        }
+
+        public synchronized void stop() {
+            for(CrawlThread t: this.threads) {
+                // interrupting the thread should be fine for us in our use-case.
+                t.t.interrupt();
             }
-        } while(this.queue.size() == 0);
+        }
 
-        synchronized (this.queue) {
-            if (this.queue.size() == 1) {
-                System.out.println("QUEUE EMPTY NOW");
+        public synchronized boolean hasNext() {
+            return this.queue.size() > 0;
+        }
+
+        public synchronized String next() {
+            if (this.queue.size() == 0) {
+                return null;
             }
             return this.queue.remove(0);
         }
-    }
 
-    public void done(String url) {
-        final VisitedURL obj = this.visited.putIfAbsent(url, new VisitedURL(url));
-        if (obj == null) {
-            this.visited.get(url).visits++;
+        private void tryNext() {
+            if (!this.hasNext() || this.threads.size() == this.maxThreads) {
+                return;
+            }
+
+            String next = this.next();
+            if (next == null) {
+                System.out.println("invalid next string");
+                return;
+            }
+
+            this.threads.add(new CrawlThread(this, next));
+        }
+
+        public void done(CrawlThread t, String url) {
+            final VisitedURL obj = this.visited.putIfAbsent(url, new VisitedURL(url));
+            if (obj == null) {
+                this.visited.get(url).visits++;
+            }
+            this.threads.remove(t);
+            this.tryNext();
+        }
+
+        public synchronized void addURL(String url) {
+            // TODO: we might want to ignore the URLs query
+            if (this.queue.contains(url)) {
+                return;
+            }
+            if (this.visited.containsKey(url)) {
+                this.visited.get(url).visits++;
+                return;
+            }
+            this.queue.add(url);
+
+            this.tryNext();
+        }
+
+        public Map<String, VisitedURL> getVisitedUrls() {
+            return visited;
         }
     }
-
-    public synchronized void addURL(String url) {
-        // TODO: we might want to ignore the URLs query
-        if (this.queue.contains(url)) {
-            return;
-        }
-        if (this.visited.containsKey(url)) {
-            this.visited.get(url).visits++;
-            return;
-        }
-        this.queue.add(url);
-        notifyAll();
-    }
-
-    public Map<String, VisitedURL> getVisitedUrls() {
-        return visited;
-    }
-}
